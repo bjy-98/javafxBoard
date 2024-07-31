b8be56e47dff376ba6f58a5393755d4febe3d3db
bjy-98 <bis05116@naver.com>
2024-07-31 16:38:18 +0900

    두번째

--- src/main/java/kroryi/javafxboard/Main.java
+++ src/main/java/kroryi/javafxboard/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LOGIN.fxml"));
-
+//메인고침
         Scene scene = new Scene(fxmlLoader.load(), 880, 600);
         String css = getClass().getResource("/kroryi/javafxboard/Css.css").toExternalForm();
         scene.getStylesheets().add(css);
